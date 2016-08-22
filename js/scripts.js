47347b0978d43a8fc68541381517b0a9b903f7c1
Angie Smith and Nhat Hoang <angie@>
2016-08-22 16:58:20 -0700

    add function to add and delete tasks according to user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,35 @@
 //Back-end
+function Chore(task, name, time) {
+  this.taskName = task;
+  this.personName = name;
+  this.dateName = time;
+}
 
 //Front-end
 $(document).ready(function() {
-
-
+  $("form#inputTask").submit(function(){
     event.preventDefault();
+    var nameTask = $("input#whatName").val();
+    var personTask = $("input#whoTask").val();
+    var timeTask = $("input#whenTask").val();
+
+    var newTasks = new Chore(nameTask, personTask, timeTask);
+    $("ul#taskListOutput").append("<li>" + newTasks.taskName +"</li>");
+
+    $("input#whatName").val("");
+    $("input#whoTask").val("");
+    $("input#whenTask").val("");
+
+    $("#taskListOutput").last().click(function(){
+      $("#taskDisplay").show();
+      $(".inputtedPerson").text(newTasks.personName);
+      $(".inputtedTask").text(newTasks.taskName);
+      $(".inputtedTime").text(newTasks.dateName);
+    });
+    $("#taskListOutput").dblclick(function() {
+      $(this).remove();
+      $("#taskDisplay").hide();
+    })
+
   });
 });
