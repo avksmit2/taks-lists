8344be26671cca4eed1f5967eccfe45e76b0374f
Angie Smith and Zachary Matthewstearn <angie@>
2016-08-23 16:57:27 -0700

    changes delete functionality to a prototype.

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 //Back-end
+var chores = [];
+
 function Chore(task, name, time) {
   this.taskName = task;
   this.personName = name;
   this.dateName = time;
+  this.done = false;
+}
+
+Chore.prototype.choreRemove = function() {
+  if(this.done){
+    chores.splice(chores.indexOf(this), 1);
+  }
 }
 
 //Front-end
@@ -14,7 +23,10 @@ $(document).ready(function() {
     var timeTask = $("input#whenTask").val();
 
     var newTasks = new Chore(nameTask, personTask, timeTask);
-    $("ul#taskListOutput").append("<li><span class='chore'>" + newTasks.taskName +"</span></li>");
+
+    chores.push(newTasks);
+
+    $("ul#taskListOutput").append("<li><span class='chore'>" + newTasks.taskName + "</span>   <input type='checkbox' class='taskComplete'</li>");
 
     $("input#whatName").val("");
     $("input#whoTask").val("");
@@ -26,7 +38,35 @@ $(document).ready(function() {
       $(".inputtedTask").text(newTasks.taskName);
       $(".inputtedTime").text(newTasks.dateName);
     });
+    $(".taskComplete").last().change(function(){
+      newTasks.done = $(this).prop("checked");
+    });
+
+    $("button#delete").click(function() {
+      chores.forEach(function(chore){
+        chore.choreRemove();
+      });
+
+      $("#taskListOutput li").remove();
+      chores.forEach(function(chore){
+        $("ul#taskListOutput").append("<li><span class='chore'>" + chore.taskName + "</span>   <input type='checkbox' class='taskComplete'</li>");
+        $("#taskDisplay").hide();
+
+
+        $("#taskListOutput li").last().click(function(){
+          $("#taskDisplay").show();
+          $(".inputtedPerson").text(newTasks.personName);
+          $(".inputtedTask").text(newTasks.taskName);
+          $(".inputtedTime").text(newTasks.dateName);
+        });
+        $(".taskComplete").last().change(function(){
+          newTasks.done = $(this).prop("checked");
+        });
+      });
+    });
+
     $("#taskListOutput li").dblclick(function() {
+      var bob = $(this);
       $(this).remove();
       $("#taskDisplay").hide();
     })
